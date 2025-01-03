1a89badc0215f1dcb8391693b1818d04b6114650
Mohusen026 <141499445+Mohusen026@users.noreply.github.com>
2025-01-03 10:56:00 +0000

    datatype summary

---
+++ .devcontainer/01_Basic/datatype-summary.js
@@ -0,0 +1,31 @@
+// Primitive
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol,Biglnt
+
+
+
+let id = Symbol('123')
+let anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber=2564785465146514411n
+console.log(typeof bigNumber);
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros =["shaktiman","naagraj","doga"]
+{
+    name:"hitesh",
+    age:22
+}
+
+const myFunction = function(){    
+    console. log("Hello world");
+}
+
+console.log(typrof(myFunction))
+// https://262.ecma-international.org/5.1/#sec-11.4.3
