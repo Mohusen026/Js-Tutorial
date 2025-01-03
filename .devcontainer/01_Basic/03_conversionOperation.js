1933f222d118aa3260e596f56e8fa3ff666b0b92
Mohusen026 <141499445+Mohusen026@users.noreply.github.com>
2025-01-03 09:47:12 +0000

    operation

--- .devcontainer/01_Basic/03_conversionOperation.js
+++ .devcontainer/01_Basic/03_conversionOperation.js
@@ -16,12 +16,47 @@
 // true=> 1 =>; false =>0
 
 
-let isLoggedIn=1
+// let isLoggedIn=1
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => true ; 0=> false
 // ""(empty string)
 // "hello" => true
+
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+
+
+// *************************Operation **************************
+
+let value =3
+let negValue=-value
+// console.log(negValue);
+ 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**5);
+// console.log(2%2);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
