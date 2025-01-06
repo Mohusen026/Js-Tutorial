530eb19355e1ef986c3254ad299fc21ecaf31ffd
Mohusen026 <141499445+Mohusen026@users.noreply.github.com>
2025-01-06 05:18:18 +0000

    Math and num

---
+++ .devcontainer/01_Basic/066_num_math.js
@@ -0,0 +1,35 @@
+let score=10
+console.log(score);
+
+// let balance=new Number(265);
+// console.log(balance);
+
+// console. log (balance.toString().length) ;
+// console. log (balance.toFixed(2));
+
+// const otherNumber=123.8966  
+// console. log (otherNumber.toPrecision(4));
+
+// const hundreds=1000000
+// console. log (hundreds . toLocaleString('en-IN'));
+
+
+// ++++++++++++++++Maths++++++++++++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-8)); //with use of the abs abs means the absoluted value is converted the negative value into the positive and in the positive that is not affected anything on it
+
+console.log(Math.round(5.69));
+console.log(Math.ceil(9.1)); // ceil mai top value consider hoti hai after the point if you write the .1 that time it converted into the round figure
+console.log(Math.floor(6.9));// floor mai bottom value cinsider hoti hai
+
+console.log(Math.random());// random methhod mai value 0 to 1 mai exist karegi
+console.log((Math.random()*10)+1);
+
+let min=10
+let max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);//22.55 min see again
+
+
+  
---
+++ .devcontainer/01_Basic/06_num&math.js
@@ -0,0 +1,6 @@
+let score=10
+console.log(score);
+
+let id=new Number(265);
+console.log(id);
+
