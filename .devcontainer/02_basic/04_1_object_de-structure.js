e679717aa1a0f603ac5e431daadc4097f775c8fe
Mohusen026 <141499445+Mohusen026@users.noreply.github.com>
2025-01-06 10:33:45 +0000

    json

---
+++ .devcontainer/02_basic/04_1_object_de-structure.js
@@ -0,0 +1,31 @@
+const course={
+    coursename:"Js course",
+    price:850,
+    courseInstructor:"Husen"
+}
+
+const{courseInstructor:Instructor}=course
+// const{courseInstructor:hello}=course
+// console.log(hello);
+console.log(Instructor);
+
+
+/* //these is the React Code not a javascript code
+const navbar =({company})=>{
+    }
+navbar(company="Impero")
+*/
+
+
+{
+//     "name": "husen" ,
+//     "coursename": "j sin hindi" 
+//     "price": "free"
+
+// }
+
+// [
+//     {},
+//     {},
+//     {},
+// ]
