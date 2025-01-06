f109ecee57f197591aa9edd5e2d61cf627977359
Mohusen026 <141499445+Mohusen026@users.noreply.github.com>
2025-01-06 11:35:20 +0000

    function

--- .devcontainer/03_basic/01_functions.js
+++ .devcontainer/03_basic/01_functions.js
@@ -8,10 +8,36 @@ const result= addTwoNumbers(8,4)
 
 function loginUserMessage(username="karan") {
         if(!username) {
-            console.log("please enter a username");
+            // console.log("please enter a username");
             return
         } 
         return `${username} just logged In`
 }
 
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+// function calculateCartPrice(...num1)
+// {
+//     return num1
+// }
+
+// console.log(calculateCartPrice(550,874,482));
+
+function calculateCartPrice(val1,val2,...num1)
+{
+    return num1
+}
+
+console.log(calculateCartPrice(550,874,482,1400));
+
+
+const user={
+    username:"haji",
+    price:485
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);    
+}
+
+handleObject(user)
