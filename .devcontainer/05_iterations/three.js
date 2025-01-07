2b679c2a7005d29bf7bdc3061a2e3f75d52d757d
Mohusen026 <141499445+Mohusen026@users.noreply.github.com>
2025-01-07 09:51:27 +0000

    forof loop   || forof loop for string || Maps

---
+++ .devcontainer/05_iterations/three.js
@@ -0,0 +1,41 @@
+// +++++++++++++  for of loop  ++++++++++++++++++++++++
+
+let arr=[10,20,33,40,50]
+for (const num of arr) {
+    // console.log(num);    
+}
+
+// ======= for string==========
+let str="Hello Husen!"
+for (const greet of str) {
+    if (greet == " "){
+        continue         
+    }
+//    console.log(`Each Character are: ${greet}`); 
+}
+
+
+// ========= Maps =========
+
+const map=new Map()
+map.set("IN","Indian")
+map.set("USA","United State of America")
+map.set("TUR","Turkey")
+
+// for (const key of map) {
+//     console.log(key);    
+// }
+
+for (const [key,value] of map) {
+    // console.log(key,":-",value);    
+}
+
+const myObj={
+    "game1":"Mario",
+    "game2":"Spideman",
+    "game3":"Cricket"
+}
+
+for (const [key,value] of myObj) {
+    // console.log(key,":-",value);  // It show error beacuse the myObj is not iterable  
+}
