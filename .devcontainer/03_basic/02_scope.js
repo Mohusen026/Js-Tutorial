2b655e6dbc6634332d516bd2927dd80955979a0e
Mohusen026 <141499445+Mohusen026@users.noreply.github.com>
2025-01-07 04:40:20 +0000

    scope new

--- .devcontainer/03_basic/02_scope.js
+++ .devcontainer/03_basic/02_scope.js
@@ -4,10 +4,36 @@ if (true) {
     let a= 10
     const b= 20
     var c = 30
-    console.log("INNER:",a);
+    // console.log("INNER:",a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username="husen"
+    
+    function two(){
+        const websit="youtube"
+        // console.log(username);  
+    }
+   
+    // console.log (websit) ;
+    
+    two()
+}
+one()
+
+
+if (true) {
+    const username="husen"
+    if (username==="husen"){
+        const website=" youtube"
+        // console.log(username + website) ;
+    }
+// console.log(website);
+}
+// console.log(username) ;
